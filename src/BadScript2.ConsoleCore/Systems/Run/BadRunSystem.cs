894e93c5a09d1a21b06751e01b6a3ae15a9c42b4
Tim Akermann <tim.akermann@ewu-it.de>
2022-11-07 08:54:42 +0100

    Fix Warning

--- src/BadScript2.ConsoleCore/Systems/Run/BadRunSystem.cs
+++ src/BadScript2.ConsoleCore/Systems/Run/BadRunSystem.cs
@@ -29,7 +29,7 @@ namespace BadScript2.ConsoleCore.Systems.Run;
 public class BadRemoteConsoleSystemSettings
 {
     [Value(0, Default = "localhost", HelpText = "The Host to connect to", Required = true)]
-    public string Host { get; set; }
+    public string Host { get; set; } = "localhost";
     
     [Value(1, Default = 1337, HelpText = "The Host port to connect to", Required = true)]
     public int Port { get; set; } = 1337;
