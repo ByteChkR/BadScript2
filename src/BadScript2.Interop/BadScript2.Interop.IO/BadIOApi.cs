8d64e7067805f9e0c85cd722b9c637f0abaf00f7
Tim Akermann <tim.akermann@ewu-it.de>
2023-10-05 09:31:10 +0200

    Improve IO Interop API, Add OpenKM File System Abstraction

--- src/BadScript2.Interop/BadScript2.Interop.IO/BadIOApi.cs
+++ src/BadScript2.Interop/BadScript2.Interop.IO/BadIOApi.cs
@@ -6,6 +6,7 @@ using BadScript2.Runtime.Interop.Functions.Extensions;
 using BadScript2.Runtime.Objects;
 using BadScript2.Runtime.Objects.Functions;
 using BadScript2.Runtime.Objects.Native;
+using BadScript2.Runtime.Objects.Types;
 
 namespace BadScript2.Interop.IO;
 
@@ -14,30 +15,30 @@ namespace BadScript2.Interop.IO;
 /// </summary>
 public class BadIOApi : BadInteropApi
 {
-	/// <summary>
-	///     The FileSystem Instance
-	/// </summary>
-	private readonly IFileSystem m_FileSystem;
+    /// <summary>
+    ///     The FileSystem Instance
+    /// </summary>
+    private readonly IFileSystem m_FileSystem;
 
-	/// <summary>
-	///     Creates a new API Instance
-	/// </summary>
-	public BadIOApi() : this(BadFileSystem.Instance) { }
+    /// <summary>
+    ///     Creates a new API Instance
+    /// </summary>
+    public BadIOApi() : this(BadFileSystem.Instance) { }
 
-	/// <summary>
-	///     Creates a new API Instance
-	/// </summary>
-	/// <param name="fileSystem">File System Instance</param>
-	public BadIOApi(IFileSystem fileSystem) : base("IO")
+    /// <summary>
+    ///     Creates a new API Instance
+    /// </summary>
+    /// <param name="fileSystem">File System Instance</param>
+    public BadIOApi(IFileSystem fileSystem) : base("IO")
     {
         m_FileSystem = fileSystem;
     }
 
-	/// <summary>
-	///     Creates the "Path" Table
-	/// </summary>
-	/// <returns>Table</returns>
-	private BadTable CreatePath()
+    /// <summary>
+    ///     Creates the "Path" Table
+    /// </summary>
+    /// <returns>Table</returns>
+    private BadTable CreatePath()
     {
         BadTable t = new BadTable();
 
@@ -56,21 +57,62 @@ public class BadIOApi : BadInteropApi
         return t;
     }
 
-	/// <summary>
-	///     Combines the given paths
-	/// </summary>
-	/// <param name="arg">Paths</param>
-	/// <returns>Combined Path String</returns>
-	private BadObject Combine(BadObject[] arg)
+    /// <summary>
+    ///     Combines the given paths
+    /// </summary>
+    /// <param name="arg">Paths</param>
+    /// <returns>Combined Path String</returns>
+    private BadObject Combine(BadObject[] arg)
     {
         return Path.Combine(arg.Select(x => x.ToString()!).ToArray());
     }
 
-	/// <summary>
-	///     Creates the "Directory" Table
-	/// </summary>
-	/// <returns>Table</returns>
-	private BadTable CreateDirectory()
+    private void CreateMoveCopy(BadTable t)
+    {
+        BadInteropFunction move = new BadInteropFunction(
+            "Move",
+            (c, a) =>
+            {
+                IBadString src = (IBadString)a[0];
+                IBadString dst = (IBadString)a[1];
+                bool overwrite = a.Length == 3 && a[2] == BadObject.True;
+                m_FileSystem.Move(src.Value, dst.Value, overwrite);
+
+                return BadObject.Null;
+            },
+            false,
+            new BadFunctionParameter("src", false, true, false, null, BadNativeClassBuilder.GetNative("string")),
+            new BadFunctionParameter("dst", false, true, false, null, BadNativeClassBuilder.GetNative("string")),
+            new BadFunctionParameter("overwrite", true, true, false, null, BadNativeClassBuilder.GetNative("bool"))
+        );
+
+        BadInteropFunction copy = new BadInteropFunction(
+            "Copy",
+            (c, a) =>
+            {
+                IBadString src = (IBadString)a[0];
+                IBadString dst = (IBadString)a[1];
+                bool overwrite = a.Length == 3 && a[2] == BadObject.True;
+                m_FileSystem.Copy(src.Value, dst.Value, overwrite);
+
+                return BadObject.Null;
+            },
+            false,
+            new BadFunctionParameter("src", false, true, false, null, BadNativeClassBuilder.GetNative("string")),
+            new BadFunctionParameter("dst", false, true, false, null, BadNativeClassBuilder.GetNative("string")),
+            new BadFunctionParameter("overwrite", true, true, false, null, BadNativeClassBuilder.GetNative("bool"))
+        );
+
+
+        t.SetProperty("Move", move);
+        t.SetProperty("Copy", copy);
+    }
+
+    /// <summary>
+    ///     Creates the "Directory" Table
+    /// </summary>
+    /// <returns>Table</returns>
+    private BadTable CreateDirectory()
     {
         BadTable t = new BadTable();
 
@@ -95,10 +137,7 @@ public class BadIOApi : BadInteropApi
         );
 
 
-        t.SetFunction<string, string, bool>(
-            "Move",
-            m_FileSystem.Move
-        );
+        CreateMoveCopy(t);
 
         t.SetFunction("GetCurrentDirectory", () => m_FileSystem.GetCurrentDirectory());
         t.SetFunction<string>("SetCurrentDirectory", m_FileSystem.SetCurrentDirectory);
@@ -125,11 +164,11 @@ public class BadIOApi : BadInteropApi
         return t;
     }
 
-	/// <summary>
-	///     Creates the "File" Table
-	/// </summary>
-	/// <returns>Table</returns>
-	private BadTable CreateFile()
+    /// <summary>
+    ///     Creates the "File" Table
+    /// </summary>
+    /// <returns>Table</returns>
+    private BadTable CreateFile()
     {
         BadTable t = new BadTable();
 
@@ -196,15 +235,7 @@ public class BadIOApi : BadInteropApi
 
         t.SetFunction<string>("Delete", m_FileSystem.DeleteFile);
 
-        t.SetFunction<string, string>(
-            "Copy",
-            (i, o) =>
-            {
-                using Stream inS = m_FileSystem.OpenRead(i);
-                using Stream outS = m_FileSystem.OpenWrite(o, BadWriteMode.CreateNew);
-                inS.CopyTo(outS);
-            }
-        );
+        CreateMoveCopy(t);
 
         return t;
     }
