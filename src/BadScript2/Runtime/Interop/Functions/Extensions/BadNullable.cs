edc4d4bdaa3d1cb911161c2f9251dd52d527e4d4
Tim Akermann <tim.akermann@ewu-it.de>
2023-11-03 13:27:30 +0100

    Fix Bug in BadNullable.cs

--- src/BadScript2/Runtime/Interop/Functions/Extensions/BadNullable.cs
+++ src/BadScript2/Runtime/Interop/Functions/Extensions/BadNullable.cs
@@ -2,27 +2,30 @@ namespace BadScript2.Runtime.Interop.Functions.Extensions;
 
 public readonly struct BadNullable<T>
 {
-    public readonly Type Type;
     public readonly T? Value;
     public readonly bool HasValue;
+
     public BadNullable()
     {
-        Type = typeof(T);
         HasValue = false;
     }
 
     public BadNullable(T value)
     {
-        Type = typeof(T);
         Value = value;
         HasValue = true;
     }
-    
+
     public static implicit operator BadNullable<T>(T? value)
     {
+        if (value == null)
+        {
+            return new BadNullable<T>();
+        }
+
         return new BadNullable<T>(value);
     }
-    
+
     public static implicit operator T?(BadNullable<T> value)
     {
         return value.Value;
