2b038ec89194cbe7016e3eb48e696ed5fafd76b5
Tim Akermann <tim.akermann@ewu-it.de>
2023-11-06 10:53:07 +0100

    Fix Broken Versioning for Build Pipeline, Fix newNugetVersion.ps1 script

--- newNugetVersion.ps1
+++ newNugetVersion.ps1
@@ -1,6 +1,14 @@
-param ($version="(~){yyyy}.(~){MM}.(~){dd}.+", $sources="./src")
+param ($version="(~){yy}.(~){MM}.(~){dd}.+", $sources="./src")
 . ./build.ps1 -noProjects -noTests -writeLog
-cd build
-. ./BSShell.ps1
-cd ..
-. bs ./newNugetVersion.bs $version $sources
+
+$bs = "$pwd/build/"
+
+if ($IsWindows -eq $true) {
+    $bsFile = $bs + 'bs.exe'
+}
+else {
+    $bsFile = $bs + 'bs'
+}
+
+echo "BadScript Runtime Located at $bsFile"
+. $bsFile ./newNugetVersion.bs $version $sources
