0616272eaf7f22f624fc4180de2b5274e1a7b4fc
Tim Akermann <tim.akermann@ewu-it.de>
2023-11-22 11:15:51 +0100

    Specify doxygen executable directly

--- _docs/Generate.sh
+++ _docs/Generate.sh
@@ -1,13 +1,12 @@
 chmod +x _docs/*
 
 doxy_config="./doxy_config.cfg"
-doxygen_bin="./_docs/doxygen-1.9.8/bin/doxygen"
 
 ./_docs/Install.sh
 
 
 # Start Generating Doxy Documentation
-. $doxygen_bin $doxy_config
+./_docs/doxygen-1.9.8/bin/doxygen $doxy_config
 
 
 ls -la .
