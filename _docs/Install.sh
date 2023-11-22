2f8a1f5dd0b0ce9c2c5b99a6718d8c2ce763b666
Tim Akermann <tim.akermann@ewu-it.de>
2023-11-22 11:01:05 +0100

    Fix Execute Permissions

--- _docs/Install.sh
+++ _docs/Install.sh
@@ -2,4 +2,5 @@ sudo apt -y install graphviz
 cd _docs
 wget https://www.doxygen.nl/files/doxygen-1.9.8.linux.bin.tar.gz
 tar -xvf doxygen-1.9.8.linux.bin.tar.gz
+chmod +x doxygen-1.9.8/bin/doxygen
 cd ..
