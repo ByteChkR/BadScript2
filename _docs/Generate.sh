8ce4da1077a8ab613a79fc8a794b287752cab70b
Tim Akermann <tim.akermann@ewu-it.de>
2023-11-22 10:47:28 +0100

    Update Install Scripts

--- _docs/Generate.sh
+++ _docs/Generate.sh
@@ -1,12 +1,13 @@
 chmod +x _docs/*
 
 doxy_config="./doxy_config.cfg"
+doxygen_bin="./_docs/doxygen-1.9.8/bin/doxygen"
 
 ./_docs/Install.sh
 
 
 # Start Generating Doxy Documentation
-doxygen $doxy_config
+. $doxygen_bin $doxy_config
 
 
 ls -la .
--- _docs/Install.sh
+++ _docs/Install.sh
@@ -1,1 +1,3 @@
-sudo apt -y install graphviz doxygen
+sudo apt -y install graphviz
+wget https://www.doxygen.nl/files/doxygen-1.9.8.linux.bin.tar.gz
+tar -xvf doxygen-1.9.8.linux.bin.tar.gz
