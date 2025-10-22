b1e4121ea5e3e1243ee85e7261eb077750160ed7
Tim Akermann <tim.akermann@ewu-it.de>
2025-10-22 08:58:01 +0200

    Update Submodules

--- src/BadScript2/Common/Logging/BadLogMask.cs
+++ src/BadScript2/Common/Logging/BadLogMask.cs
@@ -5,74 +5,77 @@ namespace BadScript2.Common.Logging;
 /// </summary>
 public class BadLogMask
 {
-	/// <summary>
-	///     The Masks that are "known"
-	/// </summary>
-	private static readonly Dictionary<string, ulong> s_Masks = new Dictionary<string, ulong>
+    /// <summary>
+    ///     The Masks that are "known"
+    /// </summary>
+    private static readonly Dictionary<string, ulong> s_Masks = new()
     {
-        { "None", 0 }, { "Default", 1 },
+        { "None", 0 }, { "Default", 1 }
     };
 
-	/// <summary>
-	///     Static Helper for the "None" Mask
-	/// </summary>
-	public static readonly BadLogMask None = new BadLogMask(0);
+    /// <summary>
+    ///     Static Helper for the "None" Mask
+    /// </summary>
+    public static readonly BadLogMask None = new(0);
 
-	/// <summary>
-	///     Static Helper for the "All" Mask
-	/// </summary>
-	public static readonly BadLogMask All = new BadLogMask(~0UL);
+    /// <summary>
+    ///     Static Helper for the "All" Mask
+    /// </summary>
+    public static readonly BadLogMask All = new(~0UL);
 
-	/// <summary>
-	///     Static Helper for the "Default" Mask
-	/// </summary>
-	public static readonly BadLogMask Default = new BadLogMask(1);
+    /// <summary>
+    ///     Static Helper for the "Default" Mask
+    /// </summary>
+    public static readonly BadLogMask Default = new(1);
 
 
-	/// <summary>
-	///     The Mask Flags
-	/// </summary>
-	private readonly ulong m_Mask;
+    /// <summary>
+    ///     The Mask Flags
+    /// </summary>
+    private readonly ulong m_Mask;
 
-	/// <summary>
-	///     Creates a new Log Mask
-	/// </summary>
-	/// <param name="mask">The Mask Flags</param>
-	private BadLogMask(ulong mask)
+    /// <summary>
+    ///     Creates a new Log Mask
+    /// </summary>
+    /// <param name="mask">The Mask Flags</param>
+    private BadLogMask(ulong mask)
     {
         m_Mask = mask;
     }
 
 
-	/// <summary>
-	///     Registers a new Mask Name
-	/// </summary>
-	/// <param name="name">The name of the mask</param>
-	/// <returns>An instance of the log mask object</returns>
-	public static BadLogMask Register(string name)
+    /// <summary>
+    ///     Registers a new Mask Name
+    /// </summary>
+    /// <param name="name">The name of the mask</param>
+    /// <returns>An instance of the log mask object</returns>
+    public static BadLogMask Register(string name)
     {
-        ulong value = (ulong)1 << (s_Masks.Count - 1);
-        s_Masks.Add(name, value);
+        lock (s_Masks)
+        {
+            var value = (ulong)1 << (s_Masks.Count - 1);
+            s_Masks.Add(name, value);
 
-        return new BadLogMask(value);
+            return new BadLogMask(value);
+        }
     }
 
-	/// <summary>
-	///     Automatically converts a mask flag into a log mask object
-	/// </summary>
-	/// <param name="mask">The mask flag</param>
-	/// <returns>An instance of the log mask object</returns>
-	public static implicit operator BadLogMask(ulong mask)
+    /// <summary>
+    ///     Automatically converts a mask flag into a log mask object
+    /// </summary>
+    /// <param name="mask">The mask flag</param>
+    /// <returns>An instance of the log mask object</returns>
+    public static implicit operator BadLogMask(ulong mask)
     {
         return new BadLogMask(mask);
     }
 
-	/// <summary>
-	///     Automatically converts a mask name into a log mask object
-	/// </summary>
-	/// <param name="name">The mask Name</param>
-	/// <returns>An instance of the log mask object</returns>
-	public static implicit operator BadLogMask(string name)
+    /// <summary>
+    ///     Automatically converts a mask name into a log mask object
+    /// </summary>
+    /// <param name="name">The mask Name</param>
+    /// <returns>An instance of the log mask object</returns>
+    public static implicit operator BadLogMask(string name)
     {
         switch (name)
         {
@@ -82,15 +85,23 @@ public class BadLogMask
                 return All;
         }
 
-        return !s_Masks.ContainsKey(name) ? Register(name) : new BadLogMask(s_Masks[name]);
+        lock (s_Masks)
+        {
+            if (s_Masks.TryGetValue(name, out var mask))
+            {
+                return new BadLogMask(mask);
+            }
+        }
+
+        return Register(name);
     }
 
-	/// <summary>
-	///     Returns true if the specified number is a power of two
-	/// </summary>
-	/// <param name="number">The number to be checked</param>
-	/// <returns>True if the number is a power of two</returns>
-	private static bool IsPowerOfTwo(ulong number)
+    /// <summary>
+    ///     Returns true if the specified number is a power of two
+    /// </summary>
+    /// <param name="number">The number to be checked</param>
+    /// <returns>True if the number is a power of two</returns>
+    private static bool IsPowerOfTwo(ulong number)
     {
         if (number == 0)
         {
@@ -119,31 +130,31 @@ public class BadLogMask
     }
 
 
-	/// <summary>
-	///     Returns a combined mask of all masks provided
-	/// </summary>
-	/// <param name="masks">Masks that make up the resulting mask</param>
-	/// <returns>The resulting mask</returns>
-	public static BadLogMask GetMask(params BadLogMask[] masks)
+    /// <summary>
+    ///     Returns a combined mask of all masks provided
+    /// </summary>
+    /// <param name="masks">Masks that make up the resulting mask</param>
+    /// <returns>The resulting mask</returns>
+    public static BadLogMask GetMask(params BadLogMask[] masks)
     {
         return masks.Aggregate<BadLogMask, ulong>(0, (current, name) => current | name.m_Mask);
     }
 
-	/// <summary>
-	///     Returns the mask flags of the mask object
-	/// </summary>
-	/// <param name="mask">The mask instance</param>
-	/// <returns>Mask Flags</returns>
-	public static implicit operator ulong(BadLogMask mask)
+    /// <summary>
+    ///     Returns the mask flags of the mask object
+    /// </summary>
+    /// <param name="mask">The mask instance</param>
+    /// <returns>Mask Flags</returns>
+    public static implicit operator ulong(BadLogMask mask)
     {
         return mask.m_Mask;
     }
 
-	/// <summary>
-	///     Returns a list of masks that are contained in this mask
-	/// </summary>
-	/// <returns>List of mask names</returns>
-	public string[] GetNames()
+    /// <summary>
+    ///     Returns a list of masks that are contained in this mask
+    /// </summary>
+    /// <returns>List of mask names</returns>
+    public string[] GetNames()
     {
         if (m_Mask == None.m_Mask)
         {
@@ -157,69 +168,75 @@ public class BadLogMask
 
         if (IsPowerOfTwo(m_Mask))
         {
-            if (s_Masks.Any(x => x.Value == m_Mask))
+            lock (s_Masks)
             {
-                return new[]
+                if (s_Masks.Any(x => x.Value == m_Mask))
                 {
-                    s_Masks.First(x => x.Value == m_Mask)
-                           .Key,
-                };
+                    return new[]
+                    {
+                        s_Masks.First(x => x.Value == m_Mask)
+                            .Key
+                    };
+                }
             }
 
-            return Array.Empty<string>();
+            return [];
         }
 
-        List<string> names =
-            (from kvp in s_Masks where kvp.Value != 0 && (kvp.Value & m_Mask) == kvp.Value select kvp.Key).ToList();
+        lock (s_Masks)
+        {
+            List<string> names =
+                (from kvp in s_Masks where kvp.Value != 0 && (kvp.Value & m_Mask) == kvp.Value select kvp.Key).ToList();
 
-        return names.Count == 0 ? Array.Empty<string>() : names.ToArray();
+            return names.ToArray();
+        }
     }
 
-	/// <summary>
-	///     Returns the name of the masks that are contained in this mask
-	/// </summary>
-	/// <param name="mask">The mask instance</param>
-	/// <returns>Mask String Representation([Mask1], [Mask2], ..)</returns>
-	public static implicit operator string(BadLogMask mask)
+    /// <summary>
+    ///     Returns the name of the masks that are contained in this mask
+    /// </summary>
+    /// <param name="mask">The mask instance</param>
+    /// <returns>Mask String Representation([Mask1], [Mask2], ..)</returns>
+    public static implicit operator string(BadLogMask mask)
     {
         return string.Join(" ", mask.GetNames());
     }
 
-	/// <summary>
-	///     Returns true if this mask is contained in the other mask
-	/// </summary>
-	/// <param name="other">The other mask</param>
-	/// <returns>True if this mask is contained in other</returns>
-	public bool IsContainedIn(BadLogMask other)
+    /// <summary>
+    ///     Returns true if this mask is contained in the other mask
+    /// </summary>
+    /// <param name="other">The other mask</param>
+    /// <returns>True if this mask is contained in other</returns>
+    public bool IsContainedIn(BadLogMask other)
     {
         return (m_Mask & other.m_Mask) == m_Mask;
     }
 
-	/// <summary>
-	///     Returns true if this mask is identical to the other mask
-	/// </summary>
-	/// <param name="other">The other mask</param>
-	/// <returns>True if masks are identical</returns>
-	public bool IsExactly(BadLogMask other)
+    /// <summary>
+    ///     Returns true if this mask is identical to the other mask
+    /// </summary>
+    /// <param name="other">The other mask</param>
+    /// <returns>True if masks are identical</returns>
+    public bool IsExactly(BadLogMask other)
     {
         return m_Mask == other.m_Mask;
     }
 
-	/// <summary>
-	///     Returns true if the other mask is contained in this mask
-	/// </summary>
-	/// <param name="mask">The other mask</param>
-	/// <returns>True if the other mask is contained in this</returns>
-	public bool Contains(BadLogMask mask)
+    /// <summary>
+    ///     Returns true if the other mask is contained in this mask
+    /// </summary>
+    /// <param name="mask">The other mask</param>
+    /// <returns>True if the other mask is contained in this</returns>
+    public bool Contains(BadLogMask mask)
     {
         return mask.IsContainedIn(this);
     }
 
-	/// <summary>
-	///     Returns string representation of the mask
-	/// </summary>
-	/// <returns>String representation of the mask</returns>
-	public override string ToString()
+    /// <summary>
+    ///     Returns string representation of the mask
+    /// </summary>
+    /// <returns>String representation of the mask</returns>
+    public override string ToString()
     {
         return this;
     }
